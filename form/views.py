e159aefe04d9063c74a6b16979e187a30bf9fac1
Jo Greybill <jogreybill@gmail.com>
2012-07-18 16:57:45 -0400

    Form now writes to database

--- form/views.py
+++ form/views.py
@@ -8,30 +8,20 @@ from form.models import Submission, Logo, SubmissionForm
 # Create your views here.
 
 def index(request):
-	p = get_object_or_404(Submission)
-	return render_to_response('form/index.html', context_instance=RequestContext(request))
+	form = SubmissionForm(request.POST)
+	return render_to_response('form/index.html', {'form': form}, context_instance=RequestContext(request))
 
 def submit(request):
 	errors = []
         if request.method == 'POST':
 			
 			form = SubmissionForm(request.POST)
-			
-			if not request.POST.get('name', ''):
-				errors.append('Please enter a unit name.')
-			if not request.POST.get('requested_by', ''):
-				errors.append('Please enter your name under "Requested by."')
-			if not request.POST.get('phone', ''):
-				errors.append('Please enter a phone number.')
-			if request.POST.get('email') and '@' not in request.POST['email']:
-				errors.append('Enter a valid e-mail address.')
-			if not request.POST.get('department', ''):
-				errors.append('Please enter the name of the department/college making this logo request.')
-			if not request.POST.get('logo_type', ''):
-				errors.append('Please specify which logos you would like to generate.')
 			if not errors and form.is_valid():
+				form.save()
 				return HttpResponseRedirect('/download/')
+				
         return render_to_response('form/index.html', {
+			'form': form,
 			'errors': errors,
 			'name': request.POST.get('name', ''),
 			'requested_by': request.POST.get('requested_by', ''),
