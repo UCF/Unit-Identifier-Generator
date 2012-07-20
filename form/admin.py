e99d56b9e25737bfb7c00080774fec30700ab4e1
Jo Greybill <jogreybill@gmail.com>
2012-07-20 15:25:17 -0400

    Submissions in admin now appear in descending order; newest first

--- form/admin.py
+++ form/admin.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 class SubmissionAdmin(admin.ModelAdmin):
 	fields = ['unit_name', 'request_date', 'requester', 'phone', 'email', 'department', 'design_options']
 	date_hierarchy = 'request_date'
+	ordering = ['-request_date']
 	list_filter = ['request_date']
 	list_display = ('unit_name', 'request_date', 'requester')
 
