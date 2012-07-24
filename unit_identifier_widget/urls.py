bd488c0000e0837167d356ccfd9c11e0a43006e1
Jo Greybill <jogreybill@gmail.com>
2012-07-24 11:57:17 -0400

    Added url entry to prevent cache headers

--- unit_identifier_widget/urls.py
+++ unit_identifier_widget/urls.py
@@ -9,4 +9,7 @@ urlpatterns = patterns('',
 	url(r'^$', 'form.views.index'),
 	url(r'^download/$', 'form.views.download'),
 	url(r'^admin/', include(admin.site.urls)),
+	# static files w/ no-cache headers
+	url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
+	    {'document_root': settings.STATIC_ROOT}),
 )
