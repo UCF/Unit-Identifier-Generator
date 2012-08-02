0ce5e699af7a5721ad228ce906dca6d9ee49abf1
Jo Greybill <jogreybill@gmail.com>
2012-08-02 15:53:04 -0400

    App is now set up to use admin static files from new admin directory in static files folder

--- unit_identifier_widget/settings.py
+++ unit_identifier_widget/settings.py
@@ -51,15 +51,6 @@ USE_L10N = True
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
-
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
@@ -77,6 +68,8 @@ STATICFILES_DIRS = (
     # Don't forget to use absolute paths, not relative paths.
 	'/home/www/static/unit_identifier_widget',
 )
+
+ADMIN_MEDIA_ROOT = '/home/www/static/admin/'
 
 # List of finder classes that know how to find static files in
 # various locations.
