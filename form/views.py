d6c0319a29da6dce123b32aba32319a7c8ec56b2
Jo Greybill <jogreybill@gmail.com>
2012-08-03 11:24:19 -0400

    Added error message on front-end if a unit name has already been requested

--- form/views.py
+++ form/views.py
@@ -17,59 +17,70 @@ def index(request):
 			form = SubmissionForm(request.POST)
 			if not errors and form.is_valid():
 				
-				design_options = request.POST.getlist('design_options')
-				unit_name = request.POST.get('unit_name')				
+				requested_unit_name = request.POST.get('unit_name')	
+				match_attempt = Submission.objects.get(unit_name=requested_unit_name)
 				
-				uid_fontsize = request.POST.get('uid_fontsize')
-				muid_fontsize = request.POST.get('muid_fontsize')
-				vuid_fontsize = request.POST.get('vuid_fontsize')
+				if match_attempt != '':
+					match_exists_error = True
+					match_requester = match_attempt.requester
+					match_requester_dept = match_attempt.department
+				else:
+					match_exists_error = False
+					design_options = request.POST.getlist('design_options')			
 				
-				uid_spanw = request.POST.get('uid_span_w')
-				muid_spanw = request.POST.get('muid_span_w')
-				vuid_spanw = request.POST.get('vuid_span_w')
+					uid_fontsize = request.POST.get('uid_fontsize')
+					muid_fontsize = request.POST.get('muid_fontsize')
+					vuid_fontsize = request.POST.get('vuid_fontsize')
 				
-				uid_spanh = request.POST.get('uid_span_h')
-				muid_spanh = request.POST.get('muid_span_h')
-				vuid_spanh = request.POST.get('vuid_span_h')
+					uid_spanw = request.POST.get('uid_span_w')
+					muid_spanw = request.POST.get('muid_span_w')
+					vuid_spanw = request.POST.get('vuid_span_w')
 				
-				muid_linebreak = request.POST.get('muid_linebreak')
+					uid_spanh = request.POST.get('uid_span_h')
+					muid_spanh = request.POST.get('muid_span_h')
+					vuid_spanh = request.POST.get('vuid_span_h')
 				
-				# Designate the file path at which new logos will be saved.
-				# e.g.: generated_logos/my-unit-name-1269884900480978/MUID/MUID-my-unit-name-K.png
+					muid_linebreak = request.POST.get('muid_linebreak')
 				
-				currenttime = strftime("%Y%m%d%H%M%S", gmtime())
+					# Designate the file path at which new logos will be saved.
+					# e.g.: generated_logos/my-unit-name-1269884900480978/MUID/MUID-my-unit-name-K.png
 				
-				generated_logos_dir = settings.PROJECT_FOLDER + "/generated_logos/" + slugify(unit_name) + "-" + currenttime + "/"
-				if not os.path.exists(generated_logos_dir):
-				    os.makedirs(generated_logos_dir)
+					currenttime = strftime("%Y%m%d%H%M%S", gmtime())
 				
-				# From here, need to run process only for each selected design option...
+					generated_logos_dir = settings.PROJECT_FOLDER + "/generated_logos/" + slugify(requested_unit_name) + "-" + currenttime + "/"
+					if not os.path.exists(generated_logos_dir):
+						os.makedirs(generated_logos_dir)
 				
-				# IDGen() takes up to 7 args: 
-				# design_option, generated_logos_dir, unit_name, fontsize, spanw, spanh, muid_linebreak (optional-muid only)
+					# From here, need to run process only for each selected design option...
 				
-				if 'UID' in design_options:
-					uid_gen = IDGen("UID", generated_logos_dir, unit_name, uid_fontsize, uid_spanw, uid_spanh)
-					uid_gen.makelogos()
+					# IDGen() takes up to 7 args: 
+					# design_option, generated_logos_dir, unit_name, fontsize, spanw, spanh, muid_linebreak (optional-muid only)
+				
+					if 'UID' in design_options:
+						uid_gen = IDGen("UID", generated_logos_dir, requested_unit_name, uid_fontsize, uid_spanw, uid_spanh)
+						uid_gen.makelogos()
 					
-				if 'MUID' in design_options:				
-					muid_gen = IDGen("MUID", generated_logos_dir, unit_name, muid_fontsize, muid_spanw, muid_spanh, muid_linebreak)
-					muid_gen.makelogos()
+					if 'MUID' in design_options:				
+						muid_gen = IDGen("MUID", generated_logos_dir, requested_unit_name, muid_fontsize, muid_spanw, muid_spanh, muid_linebreak)
+						muid_gen.makelogos()
 					
-				if 'VUID' in design_options:
-					vuid_gen = IDGen("VUID", generated_logos_dir, unit_name, vuid_fontsize, vuid_spanw, vuid_spanh)
-					vuid_gen.makelogos()
+					if 'VUID' in design_options:
+						vuid_gen = IDGen("VUID", generated_logos_dir, requested_unit_name, vuid_fontsize, vuid_spanw, vuid_spanh)
+						vuid_gen.makelogos()
 				
-				form.save()
-				return render_to_response('form/download.html', {
-					'design_options': design_options
-				}, context_instance=RequestContext(request))
+					form.save()
+					return render_to_response('form/download.html', {
+						'design_options': design_options
+					}, context_instance=RequestContext(request))
 	else:
 		form = SubmissionForm()
 		
 	return render_to_response('form/index.html', {
 		'form': form,
 		'errors': errors,
+		'match_exists_error': match_exists_error,
+		'match_requester': match_requester,
+		'match_requester_dept': match_requester_dept,
 		'name': request.POST.get('name', ''),
 		'requested_by': request.POST.get('requested_by', ''),
 		'phone': request.POST.get('phone', ''),
