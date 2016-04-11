7a999198d5eef01678fa8071b16605aaba712ebe
Jo Dickson <jogreybill@gmail.com>
2016-04-11 13:40:28 -0500

    Re-added static_root; UID generator should at least function now

--- unit_identifier_widget/settings.py
+++ unit_identifier_widget/settings.py
@@ -63,7 +63,7 @@ USE_TZ = True
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-# STATIC_ROOT = '/home/www/static/unit_identifier_widget/'
+STATIC_ROOT = 'static/unit_identifier_widget/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
