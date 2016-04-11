8a1a1eb94e0398afe9591067be65c2b1058c0967
Jo Dickson <jogreybill@gmail.com>
2016-04-11 12:08:23 -0500

    Static setting updates to get the app to run locally

--- unit_identifier_widget/settings.py
+++ unit_identifier_widget/settings.py
@@ -63,29 +63,29 @@ USE_TZ = True
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/home/www/static/unit_identifier_widget/'
+# STATIC_ROOT = '/home/www/static/unit_identifier_widget/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = 'http://webcom.dev.smca.ucf.edu/static/unit_identifier_widget/'
+STATIC_URL = '/static/'
 
 # Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-	'/home/www/static/unit_identifier_widget',
-)
+# STATICFILES_DIRS = (
+#     # Put strings here, like "/home/html/static" or "C:/www/django/static".
+#     # Always use forward slashes, even on Windows.
+#     # Don't forget to use absolute paths, not relative paths.
+# 	'/home/www/static/unit_identifier_widget',
+# )
 
 ADMIN_MEDIA_ROOT = '/home/www/static/admin/'
 
 # List of finder classes that know how to find static files in
 # various locations.
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-)
+# STATICFILES_FINDERS = (
+#     'django.contrib.staticfiles.finders.FileSystemFinder',
+#     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+# #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+# )
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'u*!6e=@7oe*@-13gmahynd%74&amp;m+m*yg(h8f(*85h^f!nv+1f+'
